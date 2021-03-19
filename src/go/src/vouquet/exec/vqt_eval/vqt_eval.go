02af3f7abf9dcc9b06400fc8e50e14d3bcb7a917
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2021-03-19 16:55:18 +0900

    Updated args use static date

--- src/go/src/vouquet/exec/vqt_eval/vqt_eval.go
+++ src/go/src/vouquet/exec/vqt_eval/vqt_eval.go
@@ -8,51 +8,54 @@ import (
 	"time"
 	"context"
 	"strconv"
+
+	_ "time/tzdata"
 )
 
 import (
 	"vouquet/soil"
-	"vouquet/seed"
 )
 
 import (
 	"github.com/vouquet/florist"
 )
 
-const (
-	TEST_SIZE = 0.01
-)
-
 var (
 	Cpath    string
-	Detail   bool
-	TestDays int
+
+	Start  time.Time
+	End    time.Time
+
+	Symbol string
+	Soil   string
+	Size   float64
+
+	Detail     bool
+	VeryDetail bool
 )
 
 type logger struct {}
 
 func (self *logger) WriteMsg(s string, msg ...interface{}) {
-	if Detail {
+	if VeryDetail || Detail {
 		fmt.Fprintf(os.Stdout, "[STDOUT] " + s + "\n" , msg...)
 	}
 }
 
 func (self *logger) WriteErr(s string, msg ...interface{}) {
-	if Detail {
+	if VeryDetail || Detail {
 		fmt.Fprintf(os.Stderr, "[ERROR]" + s + "\n" , msg...)
 	}
 }
 
 func (self *logger) WriteDebug(s string, msg ...interface{}) {
-	if Detail {
+	if VeryDetail {
 		fmt.Fprintf(os.Stdout, "[DEBUG] " + s + "\n" , msg...)
 	}
 }
 
 func eval() error {
-	now := time.Now()
-	start := now.AddDate(0, 0, int(TestDays * -1))
-	before_data := start.Add(-20 * time.Minute)
+	before_data := Start.Add(-20 * time.Minute)
 
 	log := new(logger)
 	ctx := context.Background()
@@ -62,7 +65,7 @@ func eval() error {
 	}
 	defer r.Close()
 
-	status, err := r.GetStatus(soil.SOIL_GMO, seed.SYMBOL_BTC_REVERAGE, before_data, start)
+	status, err := r.GetStatus(Soil, Symbol, before_data, Start)
 	if err != nil {
 		return err
 	}
@@ -71,7 +74,7 @@ func eval() error {
 	chan_list := make(map[string]chan *soil.State)
 	for _, name := range florist.MEMBERS {
 
-		p := soil.NewTestPlanter(soil.SOIL_GMO, seed.SYMBOL_BTC_REVERAGE, log)
+		p := soil.NewTestPlanter(Soil, Symbol, log)
 		fl, err := florist.NewFlorist(name, p, status, log)
 		if err != nil {
 			return err
@@ -85,7 +88,7 @@ func eval() error {
 	var tail time.Time
 
 	go func() {
-		t_status, err := r.GetStatus(soil.SOIL_GMO, seed.SYMBOL_BTC_REVERAGE, start, now)
+		t_status, err := r.GetStatus(Soil, Symbol, Start, End)
 		if err != nil {
 			return
 		}
@@ -112,7 +115,7 @@ func eval() error {
 		go func(name string, fl florist.Florist) {
 			defer wg.Done()
 
-			if err := fl.Run(ctx, TEST_SIZE, chan_list[name]); err != nil {
+			if err := fl.Run(ctx, Size, chan_list[name]); err != nil {
 				log.WriteErr("cannot run %s, %s", name, err)
 			}
 		}(name, fl)
@@ -144,34 +147,67 @@ func die(s string, msg ...interface{}) {
 }
 
 func init() {
+	var very_detail bool
 	var detail bool
 	var c_path string
 	flag.StringVar(&c_path, "c", "./vouquet.conf", "config path.")
 	flag.BoolVar(&detail, "v", false, "display detail.")
+	flag.BoolVar(&very_detail, "vv", false, "display very very detail.")
 	flag.Parse()
 
-	if flag.NArg() < 1 {
-		die("usage : vqt_test [-c <config path>] [-v] <test days>")
+	if flag.NArg() < 5 {
+		die("usage : vqt_eval [-c <config path>] [-v|-vv] <start-date(yyyy/mm/dd)> <end-date(yyyy/mm/dd)> <SYMBOL> <SOIL> <SIZE>")
 	}
 	if flag.NFlag() < 0 {
-		die("usage : vqt_test [-c <config path>] [-v] <test days>")
+		die("usage : vqt_eval [-c <config path>] [-v|-vv] <start-date(yyyy/mm/dd)> <end-date(yyyy/mm/dd)> <SYMBOL> <SOIL> <SIZE>")
 	}
 
-	test_days, err := strconv.Atoi(flag.Arg(0))
+	st_s := flag.Arg(0)
+	et_s := flag.Arg(1)
+	symbol := flag.Arg(2)
+	soil := flag.Arg(3)
+	size_s := flag.Arg(4)
+
+	size, err := strconv.ParseFloat(size_s, 64)
 	if err != nil {
-		die("err: '%s", err)
+		die("cannot convert float64 from size: '%s'", err)
 	}
 
-	if test_days < 1 {
-		die("cannot set size of test days less than 1.")
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		die("cannot load timezone: Asia/Tokyo")
 	}
+	st_base, err := time.ParseInLocation("2006/01/02", st_s, jst)
+	if err != nil {
+		die("cannot convert time from start-date. '%s'", err)
+	}
+	st := time.Date(st_base.Year(), st_base.Month(), st_base.Day(), 0, 0, 0, 0, st_base.Location())
+
+	et_base, err := time.ParseInLocation("2006/01/02", et_s, jst)
+	if err != nil {
+		die("cannot convert time from end-date. '%s'", err)
+	}
+	et := time.Date(et_base.Year(), et_base.Month(), et_base.Day(), 23, 59, 59, 999999999, et_base.Location())
+
 	if c_path == "" {
-		die("empty path")
+		die("cannot set empty value of config path.")
+	}
+	if symbol == "" {
+		die("cannot set empty value of symbol.")
+	}
+	if soil == "" {
+		die("cannot set empty value of soil.")
 	}
 
 	Detail = detail
-	TestDays = test_days
+	VeryDetail = very_detail
 	Cpath = c_path
+	Symbol = symbol
+	Soil = soil
+	Size = size
+
+	Start = st
+	End = et
 }
 
 func main() {
