278173dce0ecbfe2c00eaa84450836fa9fdc250e
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2021-04-13 19:54:52 +0900

    added funcfion of get position

--- src/go/src/vouquet/shop/bitflyer.go
+++ src/go/src/vouquet/shop/bitflyer.go
@@ -1,5 +1,7 @@
 package shop
 
+import "log"
+
 import (
 	"fmt"
 	"time"
@@ -12,6 +14,7 @@ import (
 
 var (
 	Symbol2Bitflyer map[string]string
+	Symbol2BitflyerCurrencyKey map[string]string
 )
 
 func bitflyerErrorf(s string, msg ...interface{}) error {
@@ -25,10 +28,26 @@ func init() {
 	Symbol2Bitflyer[XRP2JPY_spt] = bitflyer.PRODUCTCODE_XRP_JPY
 	Symbol2Bitflyer[XLM2JPY_spt] = bitflyer.PRODUCTCODE_XLM_JPY
 	Symbol2Bitflyer[MONA2JPY_spt] = bitflyer.PRODUCTCODE_MONA_JPY
+
+	Symbol2BitflyerCurrencyKey = make(map[string]string)
+	Symbol2BitflyerCurrencyKey[BTC2JPY_spt] = bitflyer.CURRENCYCODE_BTC
+	Symbol2BitflyerCurrencyKey[ETH2JPY_spt] = bitflyer.CURRENCYCODE_ETH
+	Symbol2BitflyerCurrencyKey[XRP2JPY_spt] = bitflyer.CURRENCYCODE_XRP
+	Symbol2BitflyerCurrencyKey[XLM2JPY_spt] = bitflyer.CURRENCYCODE_XLM
+	Symbol2BitflyerCurrencyKey[MONA2JPY_spt] = bitflyer.CURRENCYCODE_MONA
 }
 
 func getBitflyerKey(name string) (string, error) {
 	key, ok := Symbol2Bitflyer[name]
+	if !ok {
+		return "", fmt.Errorf("cannot support '%s'", name)
+	}
+
+	return key, nil
+}
+
+func getBitflyerCurrencyKey(name string) (string, error) {
+	key, ok := Symbol2BitflyerCurrencyKey[name]
 	if !ok {
 		return "", fmt.Errorf("cannot support '%s'", name)
 	}
@@ -92,18 +111,114 @@ func (self *BitflyerHandler) GetRate() (map[string]Rate, error) {
 }
 
 func (self *BitflyerHandler) GetPositions(symbol string) ([]Position, error) {
-	return nil, bitflyerErrorf("cannot use yet")
+	key, err := getBitflyerKey(symbol)
+	if err != nil {
+		return nil, bitflyerErrorf("%s", err)
+	}
+	c_key, err := getBitflyerCurrencyKey(symbol)
+	if err != nil {
+		return nil, bitflyerErrorf("%s", err)
+	}
+
+	b, err := self.shop.GetBalance(c_key)
+	if err != nil {
+		return nil, bitflyerErrorf("%s", err)
+	}
+	no_fix_val := b.Available
+
+	o_os, err := self.shop.GetOpenOrders(key)
+	if err != nil {
+		return nil, bitflyerErrorf("%s", err)
+	}
+	for _, o := range o_os {
+		if o.Side != TYPE_SELL {
+			continue
+		}
+		no_fix_val -= float64(o.Price * o.Size)
+	}
+
+	pos := []Position{}
+	if no_fix_val < float64(0) {
+		return pos, nil
+	}
+
+	c_os, err := self.shop.GetClosedOrders(key)
+	if err != nil {
+		return nil, bitflyerErrorf("%s", err)
+	}
+	for _, order := range c_os {
+		if order.Side != TYPE_BUY {
+			continue
+		}
+
+		if no_fix_val < float64(order.Price * order.Size) {
+			break
+		}
+		pos = append(pos, &BitflyerPosition{order:order})
+		no_fix_val -= float64(order.Price * order.Size)
+	}
+
+	if no_fix_val <= float64(0) {
+		return pos, nil
+	}
+
+	rates, err := self.shop.GetRates([]string{key})
+	if err != nil {
+		return nil, bitflyerErrorf("%s", err)
+	}
+	rate, ok := rates[key]
+	if !ok {
+		return nil, bitflyerErrorf("cannot get rate.")
+	}
+
+	price := rate.Ask()
+	size := no_fix_val - (no_fix_val * bitflyer.FEE_TRADE_RATE)
+	log.Println(no_fix_val)
+	log.Printf("%.3f\n", price)
+	log.Printf("%.3f\n", size)
+	dummy_order := &bitflyer.Order{
+		Id: time.Now().Unix(),
+		Product: key,
+		Size: size,
+		Price: price,
+		Side: TYPE_BUY,
+	}
+	pos = append(pos, &BitflyerPosition{order:dummy_order})
+	log.Println(*dummy_order)
+	return pos, nil
 }
 
 func (self *BitflyerHandler) GetFixes(symbol string) ([]Fix, error) {
 	return nil, bitflyerErrorf("cannot use yet")
+	/*
+	key, err := getBitflyerKey(symbol)
+	if err != nil {
+		return nil, err
+	}
+
+	os, err := self.shop.GetClosedOrders(key)
+	if err != nil {
+		return nil, err
+	}
+
+	fs := []Fix{}
+	for _, o := range os {
+		pos = append(pos, &BitflyerPosition{order:o})
+	}
+	return fs, nil
+	*/
 }
 
 func (self *BitflyerHandler) OrderStreamIn(o_type string, symbol string, size float64) error {
+	if o_type != TYPE_BUY {
+		return fmt.Errorf("cannot support type of order '%s'", o_type)
+	}
+	//buy
 	return bitflyerErrorf("cannot use yet")
 }
 
 func (self *BitflyerHandler) OrderStreamOut(pos Position) error {
+	//sell
 	return bitflyerErrorf("cannot use yet")
 }
 
@@ -145,4 +260,28 @@ func (self *BitflyerRate) High() float64 {
 
 func (self *BitflyerRate) Low() float64 {
 	return float64(0)
+}
+
+type BitflyerPosition struct {
+	order *bitflyer.Order
+}
+
+func (self *BitflyerPosition) Id() string {
+	return fmt.Sprintf("%v", self.order.Id)
+}
+
+func (self *BitflyerPosition) Symbol() string {
+	return self.order.Product
+}
+
+func (self *BitflyerPosition) Size() float64 {
+	return self.order.Size
+}
+
+func (self *BitflyerPosition) Price() float64 {
+	return self.order.Price
+}
+
+func (self *BitflyerPosition) OrderType() string {
+	return self.order.Side
 }
